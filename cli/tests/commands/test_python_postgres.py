2743d7033d68a433ec528b5178ce16d618b7b6d0
AkhilRajSri <akhil@shurutech.com>
2024-02-28 15:01:39 +0530

    [Akhil] add test for check_hosts

--- cli/tests/commands/test_python_postgres.py
+++ cli/tests/commands/test_python_postgres.py
@@ -6,7 +6,6 @@ from unittest.mock import patch, mock_open, MagicMock
 import unittest
 from unittest.mock import call
 import yaml
-from InquirerPy import inquirer 
 
 
 class TestPythonPostgres(unittest.TestCase):
@@ -25,7 +24,7 @@ class TestPythonPostgres(unittest.TestCase):
                    }
             with patch("builtins.input", return_value=""):
                 pp.check_configs()
-            mock_confirm.assert_called_once_with(message=f"Do you want to setup a replica server? (Default= No) :: ", default=False)
+            mock_confirm.assert_called_once_with(message="Do you want to setup a replica server? (Default= No) :: ", default=False)
             mock_confirmation.execute.assert_called_once()
             self.assertEqual(pp.configs["postgresmainserver.yml"]["postgres_port"], "5432")
             calls = [call("playbooks/group_vars", group, pp.configs[group]) for group in pp.configs.keys()]
@@ -46,13 +45,65 @@ class TestPythonPostgres(unittest.TestCase):
                    }
             with patch("builtins.input", return_value="5434"):
                 pp.check_configs()
-            mock_confirm.assert_called_once_with(message=f"Do you want to setup a replica server? (Default= No) :: ", default=False)
+            mock_confirm.assert_called_once_with(message="Do you want to setup a replica server? (Default= No) :: ", default=False)
             mock_confirmation.execute.assert_called_once()
             self.assertEqual(pp.configs["postgresmainserver.yml"]["postgres_port"], "5434")
             calls = [call("playbooks/group_vars", group, pp.configs[group]) for group in pp.configs.keys()]
             mock_write_to_file.assert_has_calls(calls, any_order=True)
+    
+    @patch("cli.src.commands.python_postgres.write_to_file")    
+    def test_check_hosts_with_user_input_values(self, mock_write_to_file):
+        with patch("InquirerPy.inquirer.confirm") as mock_confirm:
+            mock_confirmation = MagicMock()
+            mock_confirm.return_value = mock_confirmation
+            mock_confirmation.execute.return_value = False
+            pp = PythonPostgres(environment="staging")
+            pp.hosts ={
+                            'postgresmainserver': 
+                             {
+                                 'hosts': 
+                                    {
+                                        'mainserver': 
+                                        {
+                                            'ansible_connection': '5432'
+                                        }
+                                    }
+                            }
+                      }
+            with patch("builtins.input", return_value="5434"):
+                pp.check_hosts()
+            mock_confirm.assert_called_once_with(message="Do you want to setup a replica server? (Default= No) :: ", default=False)
+            mock_confirmation.execute.assert_called_once()
+            self.assertEqual(pp.hosts["postgresmainserver"]["hosts"]["mainserver"]["ansible_connection"], "5434")
+            calls = [call("inventories/staging", "hosts.yml", pp.hosts)]
+            mock_write_to_file.assert_has_calls(calls, any_order=True)
 
-
+    @patch("cli.src.commands.python_postgres.write_to_file")    
+    def test_check_hosts_with_default_values(self, mock_write_to_file):
+        with patch("InquirerPy.inquirer.confirm") as mock_confirm:
+            mock_confirmation = MagicMock()
+            mock_confirm.return_value = mock_confirmation
+            mock_confirmation.execute.return_value = False
+            pp = PythonPostgres(environment="staging")
+            pp.hosts ={
+                        'postgresmainserver': 
+                            {
+                                'hosts': 
+                                    {
+                                        'mainserver': 
+                                            {
+                                                'ansible_connection': '5432'
+                                            }
+                                    }
+                            }
+                        }
+            with patch("builtins.input", return_value=""):
+                pp.check_hosts()
+            mock_confirm.assert_called_once_with(message="Do you want to setup a replica server? (Default= No) :: ", default=False)
+            mock_confirmation.execute.assert_called_once()
+            self.assertEqual(pp.hosts["postgresmainserver"]["hosts"]["mainserver"]["ansible_connection"], "5432")
+            calls = [call("inventories/staging", "hosts.yml", pp.hosts)]
+            mock_write_to_file.assert_has_calls(calls, any_order=True)
 class TestPythonPostgresUtility(unittest.TestCase):
     def test_get_input_user_with_user_input(self):
         with patch("builtins.input", return_value="10.10.09.01/67"):
