ae9d22e6106ad2b6d990180f9f52c6093dd503e1
AkhilRajSri <akhil@shurutech.com>
2024-03-06 12:50:17 +0530

    [Akhil|Rageshwari] add write_to_configuration method to golang class

--- cli/src/webserver/golang.py
+++ cli/src/webserver/golang.py
@@ -1,4 +1,4 @@
-from src.utils.utils import node_configuration_parameters, read_from_file
+from src.utils.utils import node_configuration_parameters, read_from_file, write_to_file
 
 config_dir = "playbooks/group_vars"
 class Golang:
@@ -10,3 +10,7 @@ class Golang:
 
     def parameter_configuration(self):
         Golang.configs = node_configuration_parameters(Golang.configs)
+
+    def write_configuration_to_file(self):
+        for config_file in Golang.config_files:
+            write_to_file(config_dir, config_file, Golang.configs[config_file])
--- cli/tests/webserver/test_golang.py
+++ cli/tests/webserver/test_golang.py
@@ -1,6 +1,6 @@
 from src.webserver.golang import Golang
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, call
 from src.utils.utils import node_configuration_parameters
 
 class TestGolang(unittest.TestCase):
@@ -25,3 +25,17 @@ class TestGolang(unittest.TestCase):
         with patch("builtins.input", return_value="8081"):
             golang.configs = node_configuration_parameters(golang.configs)
         self.assertEqual(golang.configs["golangwebservers.yml"]["golang_port"], "8081")
+    
+    @patch("src.webserver.golang.write_to_file")
+    def test_write_configuration_parameters_called_with_expected_arguments(self, mock_write_to_file):
+        golang = Golang()
+        Golang.configs = {
+            "golangwebservers.yml": {
+                "golang_version": "1.21.3"
+            }
+        }
+        golang.write_configuration_to_file()
+        actual_call = mock_write_to_file.call_args
+        expected_call = call('playbooks/group_vars', 'golangwebservers.yml', {'golang_version': Golang.configs["golangwebservers.yml"]["golang_version"]})
+        self.assertEqual(actual_call, expected_call)
+        
