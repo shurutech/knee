acd91018675e1e68aab62b744e74ad3cf99be857
AkhilRajSri <akhil@shurutech.com>
2024-03-06 13:32:30 +0530

    [Akhil|Rageshwari] add write_to_configuration method to Nodejs class

--- cli/src/webserver/nodejs.py
+++ cli/src/webserver/nodejs.py
@@ -1,4 +1,4 @@
-from src.utils.utils import node_configuration_parameters, read_from_file
+from src.utils.utils import node_configuration_parameters, read_from_file, write_to_file
 config_dir = "playbooks/group_vars"
 class Nodejs:
     config_files = ["nodewebservers.yml"]
@@ -9,3 +9,9 @@ class Nodejs:
 
     def parameter_configuration(self):
         Nodejs.configs = node_configuration_parameters(Nodejs.configs)
+
+    def write_configuration_to_file(self):
+        for config_file in Nodejs.config_files:
+            write_to_file(config_dir, config_file, Nodejs.configs[config_file])    
+   
+    
--- cli/tests/webserver/test_nodejs.py
+++ cli/tests/webserver/test_nodejs.py
@@ -1,6 +1,6 @@
 from src.webserver.nodejs import Nodejs
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, call
 from src.utils.utils import node_configuration_parameters
 
 class TestNodejs(unittest.TestCase):
@@ -25,4 +25,17 @@ class TestNodejs(unittest.TestCase):
         with patch("builtins.input", return_value="8081"):
             nodejs.configs = node_configuration_parameters(nodejs.configs)
         self.assertEqual(nodejs.configs["nodejswebservers.yml"]["nodejs_port"], "8081")
+
+    @patch("src.webserver.nodejs.write_to_file")
+    def test_write_configuration_parameters_called_with_expected_arguments(self, mock_write_to_file):
+        nodejs = Nodejs()
+        Nodejs.configs = {
+            "nodewebservers.yml": {
+                "nodejs_version": "1.21.3"
+            }
+        }
+        nodejs.write_configuration_to_file()
+        actual_call = mock_write_to_file.call_args
+        expected_call = call('playbooks/group_vars', 'nodewebservers.yml', {'nodejs_version': Nodejs.configs["nodewebservers.yml"]["nodejs_version"]})
+        self.assertEqual(actual_call, expected_call)
     
