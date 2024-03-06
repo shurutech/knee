268fe0b9f0f134cb2cfc2b703a7678904b7e8d83
AkhilRajSri <akhil@shurutech.com>
2024-03-06 18:30:00 +0530

    [Akhil|Rageshwari] add write_configuration_to_file method to python class

--- cli/src/webserver/python.py
+++ cli/src/webserver/python.py
@@ -1,12 +1,16 @@
-from src.utils.utils import node_configuration_parameters, read_from_file
+from src.utils.utils import node_configuration_parameters, read_from_file, write_to_file
 config_dir = "playbooks/group_vars"
 class Python:
     config_files = ["pythonwebservers.yml"]
     configs = {}
     def __init__(self):
-        for config_file in Python.config_files:
-                Python.configs[config_file] = read_from_file(config_dir, config_file)
+        for config_file in self.config_files:
+                self.configs[config_file] = read_from_file(config_dir, config_file)
 
     def parameter_configuration(self):
-        Python.configs = node_configuration_parameters(Python.configs)
+        self.configs = node_configuration_parameters(self.configs)
+    
+    def write_configuration_to_file(self):
+        for config_file in self.config_files:
+            write_to_file(config_dir, config_file, self.configs[config_file])
 
--- cli/tests/webserver/test_python.py
+++ cli/tests/webserver/test_python.py
@@ -1,7 +1,7 @@
 from src.webserver.python import Python
 from src.utils.utils import node_configuration_parameters
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, call
 
 class TestPython(unittest.TestCase):
     def test_parameter_configuration_with_default_values(self):
@@ -25,5 +25,18 @@ class TestPython(unittest.TestCase):
         with patch("builtins.input", return_value="5434"):
             python.configs = node_configuration_parameters(python.configs)
         self.assertEqual(python.configs["pythonwebservers.yml"]["python_port"], "5434")
+
+    @patch("src.webserver.python.write_to_file")
+    def test_write_configuration_parameters_called_with_expected_arguments(self, mock_write_to_file):
+        python = Python()
+        Python.configs = {
+            "pythonwebservers.yml": {
+                "python_version": "3.8.1"
+            }
+        }
+        python.write_configuration_to_file()
+        actual_call = mock_write_to_file.call_args
+        expected_call = call('playbooks/group_vars', 'pythonwebservers.yml', {'python_version': Python.configs["pythonwebservers.yml"]["python_version"]})
+        self.assertEqual(actual_call, expected_call)
 
     
