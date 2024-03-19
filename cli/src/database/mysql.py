dd0f09923c79b8b74ca12dcb20570549bfc4a277
AkhilRajSri <akhil@shurutech.com>
2024-03-19 16:11:01 +0530

    [Akhil|Rageshwari] update configuration handling, add environment and run_playbook method to mysql class

--- cli/src/database/mysql.py
+++ cli/src/database/mysql.py
@@ -1,21 +1,30 @@
 from src.utils.utils import load_configuration
 from src.utils.file_manager import FileManager
+from src.utils.runner import run_playbook
 
 config_dir = "playbooks/group_vars"
 
 
 class Mysql:
-    config_files = ["mysqlmainserver.yml"]
-    configs = {}
+    CONFIG_FILES = ["mysqlmainserver.yml"]
+    REPLICA_CONFIG_FILES = ["mysqlmainserver.yml", "mysqlreplicaservers.yml"]
 
-    def __init__(self, replica_server_acceptance=False):
+    def __init__(self, replica_server_acceptance=False, environment="local"):
+        self.configs = {}
+        self.config_files = self.CONFIG_FILES if not replica_server_acceptance else self.REPLICA_CONFIG_FILES
         self.file_manager = FileManager()
-        if replica_server_acceptance:
-            Mysql.config_files.append("mysqlreplicaservers.yml")
-        for config_file in Mysql.config_files:
-            Mysql.configs[config_file] = self.file_manager.read_from_file(
+        self.environment = environment
+        for config_file in self.CONFIG_FILES:
+            self.configs[config_file] = self.file_manager.read_from_file(
                 config_dir, config_file
             )
 
     def parameter_configuration(self):
-        Mysql.configs = load_configuration(Mysql.configs)
+        self.configs = load_configuration(self.configs)
+
+    def write_configuration_and_run_playbook(self):
+        for config_file in self.CONFIG_FILES:
+            self.file_manager.write_to_file(
+                config_dir, config_file, self.configs[config_file]
+            )
+        run_playbook("mysql_server.yml", self.environment)
--- cli/tests/database/test_mysql.py
+++ cli/tests/database/test_mysql.py
@@ -1,31 +1,85 @@
-from src.database.mysql import Mysql
-from src.utils.utils import load_configuration
 import unittest
 from unittest.mock import patch
-
+from src.database.mysql import Mysql
 
 class TestMysql(unittest.TestCase):
-    def test_parameter_configuration_with_default_values(self):
-        mysql = Mysql()
-        mysql.configs = {"mysqlmainserver.yml": {"mysql_port": "3306"}}
-        with patch("builtins.input", return_value=""):
-            mysql.configs = load_configuration(mysql.configs)
-        self.assertEqual(mysql.configs["mysqlmainserver.yml"]["mysql_port"], "3306")
-
-    def test_parameter_configuration_with_user_input(self):
-        mysql = Mysql()
-        mysql.configs = {"mysqlmainserver.yml": {"mysql_port": "3306"}}
-        with patch("builtins.input", return_value="3307"):
-            mysql.configs = load_configuration(mysql.configs)
-        self.assertEqual(mysql.configs["mysqlmainserver.yml"]["mysql_port"], "3307")
-
-    def test_parameter_configuration_with_replica_server_acceptance(self):
-        mysql = Mysql(True)
-        mysql.configs = {
-            "mysqlwebservers.yml": {"mysql_port": "3306"},
-            "mysqlreplicaservers.yml": {"mysql_port": "3306"},
+    @patch("src.database.mysql.FileManager.read_from_file")
+    def test_initialiser_with_file(self, mock_read_from_file):
+        mock_read_from_file.return_value = {
+            "mysqlmainserver.yml": {"mysql_port": "3307"}
         }
-        with patch("builtins.input", side_effect=["3307", "3308"]):
-            mysql.configs = load_configuration(mysql.configs)
-        self.assertEqual(mysql.configs["mysqlwebservers.yml"]["mysql_port"], "3307")
-        self.assertEqual(mysql.configs["mysqlreplicaservers.yml"]["mysql_port"], "3308")
+        mysql = Mysql()
+        self.assertEqual(
+            mysql.configs["mysqlmainserver.yml"]["mysqlmainserver.yml"]["mysql_port"],
+            "3307",
+        )
+
+    @patch("src.database.mysql.load_configuration")
+    def test_parameter_configuration(self, mock_load_configuration):
+        mysql = Mysql()
+        mysql.configs = {
+            "mysqlmainserver.yml": {"mysql_port": "3305"}
+        }
+        mock_load_configuration.return_value = {
+            "mysqlmainserver.yml": {"mysql_port": "3307"}
+        }
+        mysql.parameter_configuration()
+        mock_load_configuration.assert_called_once_with( {
+            "mysqlmainserver.yml": {"mysql_port": "3305"}
+        })
+        self.assertEqual(
+              mysql.configs["mysqlmainserver.yml"]["mysql_port"],
+              "3307",
+         )
+
+    @patch("src.database.mysql.load_configuration")
+    def test_parameter_configuration_raises_error(self, mock_load_configuration):
+        mysql = Mysql()
+        mysql.configs = {
+            "mysqlmainserver.yml": {"mysql_port": "3305"}
+        }
+        mock_load_configuration.side_effect = Exception("Error")
+        with self.assertRaises(Exception) as context:
+            mysql.parameter_configuration()
+        self.assertTrue('Error' in str(context.exception))
+
+    @patch("src.database.mysql.FileManager.write_to_file")
+    @patch("src.database.mysql.run_playbook")
+    def test_write_configuration_and_run_playbook(self, mock_run_playbook, mock_write_to_file):
+        mysql = Mysql()
+        mysql.configs = {
+            "mysqlmainserver.yml": {"mysql_port": "3305"}
+        }
+        mysql.write_configuration_and_run_playbook()
+        mock_write_to_file.assert_called_once_with(
+            "playbooks/group_vars", "mysqlmainserver.yml", {"mysql_port": "3305"}
+        )
+        mock_run_playbook.assert_called_once_with("mysql_server.yml", "local")
+
+    @patch("src.database.mysql.FileManager.write_to_file")
+    @patch("src.database.mysql.run_playbook")
+    def test_write_configuration_and_run_playbook_when_write_to_file_raises_error(self, mock_run_playbook, mock_write_to_file):
+        mysql = Mysql()
+        mysql.configs = {
+            "mysqlmainserver.yml": {"mysql_port": "3305"}
+        }
+        mock_write_to_file.side_effect = Exception("Error")
+        with self.assertRaises(Exception) as context:
+            mysql.write_configuration_and_run_playbook()
+        self.assertTrue('Error' in str(context.exception))
+        mock_run_playbook.assert_not_called()
+    
+    @patch("src.database.mysql.FileManager.write_to_file")
+    @patch("src.database.mysql.run_playbook")
+    def test_write_configuration_and_run_playbook_when_run_playbook_raises_error(self, mock_run_playbook, mock_write_to_file):
+        mysql = Mysql()
+        mysql.configs = {
+            "mysqlmainserver.yml": {"mysql_port": "3305"}
+        }
+        mock_run_playbook.side_effect = Exception("Error")
+        with self.assertRaises(Exception) as context:
+            mysql.write_configuration_and_run_playbook()
+        self.assertTrue('Error' in str(context.exception))
+        mock_write_to_file.assert_called_once_with(
+            "playbooks/group_vars", "mysqlmainserver.yml", {"mysql_port": "3305"}
+        )
