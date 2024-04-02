3f8cc13614eab315cb2e404e3a4793afc55164b8
RageshwariR <rageshwari@shurutech.com>
2024-04-02 14:00:12 +0530

    [Akhil|Rageshwari] Update command selection and input handling in main.py

--- cli/src/main.py
+++ cli/src/main.py
@@ -4,27 +4,10 @@ from commands.python_postgres import PythonPostgres
 from commands.node_mongo import NodeMongo
 from commands.golang_mongo import GolangMongo
 from commands.custom_selections import CustomSelections
-from command_class_mapping import COMMAND_TO_CLASS_MAP
+from command_class_mapping import COMMAND_TO_CATEGORY_MAP
+from input_selection import custom_selections, get_environment, initial_input_selection
 
 app = typer.Typer()
-
-def custom_selections():
-    db = inquirer.select(
-        message="Select database:",
-        choices=["postgresql", "mongodb", "mysql",None],
-        default="postgres",
-    ).execute()
-    backend = inquirer.select(
-        message="Select backend:",
-        choices=["python", "nodejs", "golang","ruby",None],
-        default="python",
-    ).execute()
-    additional_services = inquirer.select(
-        message="Select additional services:",
-        choices=["redis",None],
-        default="redis",
-    ).execute()
-    return db, backend, additional_services
 
 @app.command()
 def list_command():
@@ -44,26 +27,17 @@ def describe(command: str):
 
 @app.command()
 def execute():
-    option = inquirer.select(
-        message="Please select an option:",
-        choices=["knee-defaults", "custom-selections"],
-    ).execute()
+    option = initial_input_selection()
+    server = db = additional_service = None
     if option == "knee-defaults":
         command = inquirer.select(
             message="Please select a command:",
-            choices=list(COMMAND_TO_CLASS_MAP.keys()),
+            choices=list(COMMAND_TO_CATEGORY_MAP.keys()),
             default="python-postgres",
         ).execute()
-
-        environment= inquirer.select(
-        message="Select environment to execute command:",
-        choices=["local","staging", "production"],
-        default="Staging",
-        ).execute()
-
-        if COMMAND_TO_CLASS_MAP.get(command):
-            command_class = COMMAND_TO_CLASS_MAP[command]
-            command_class(environment = environment).check_defaults()
+        if COMMAND_TO_CATEGORY_MAP.get(command):
+            server = COMMAND_TO_CATEGORY_MAP[command]["server"]
+            db = COMMAND_TO_CATEGORY_MAP[command]["db"]
         else:
             typer.secho("We are working hard for this command to be available soon!....", bg=typer.colors.YELLOW, fg=typer.colors.WHITE, bold=True)
     elif option == "custom-selections":
@@ -71,16 +45,12 @@ def execute():
         if db is None and server is None and additional_service is None:
             typer.secho("Please select a valid option", bg=typer.colors.RED, fg=typer.colors.WHITE, bold=True)
             raise typer.Abort()
-        environment= inquirer.select(
-        message="Select environment to execute command:",
-        choices=["local","staging", "production"],
-        default="staging",
-        ).execute()
-        custom_inputs = CustomSelections(environment=environment, db_client_class=db, server_class=server, additional_service=additional_service)
-        custom_inputs.check_defaults()
-        typer.echo(f"Setting up {db} database with {server} server and client...")
     else:
         typer.secho("We are working hard for this command to be available soon!....", bg=typer.colors.YELLOW, fg=typer.colors.WHITE, bold=True)
-
+    environment = get_environment()
+    custom_inputs = CustomSelections(environment=environment, db_client_class=db, server_class=server, additional_service=additional_service)
+    custom_inputs.check_defaults()
+    typer.echo("Done!")
+    
 if __name__ == "__main__":
     app()
