62973b941a58423ea5c76d50112f2b2e837cac03
RageshwariR <rageshwari@shurutech.com>
2024-04-02 17:45:21 +0530

    [Akhil|Rageshwari] Remove command class mapping file

--- cli/src/command_class_mapping.py
+++
@@ -1,6 +0,0 @@
-COMMAND_TO_CATEGORY_MAP = {
-    "python-postgres": {"server": "python", "db": "postgres"},
-    "node-mongo": {"server": "node", "db": "mongo"},
-    "golang-mongo": {"server": "golang", "db": "mongo"},
-    "ruby-mysql": {"server": "ruby", "db": "mysql"},
-}
