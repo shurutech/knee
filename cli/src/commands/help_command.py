d9f64b72abfee12fca13d59b9186063d7d5381fe
RageshwariR <rageshwari@shurutech.com>
2024-04-04 16:51:35 +0530

    [Akhil|Rageshwari] Refactor help command to display options and commands

--- cli/src/commands/help_command.py
+++ cli/src/commands/help_command.py
@@ -1,16 +1,28 @@
 import typer
-from constants import COMMAND_WITH_DESCRIPTION
+from constants import SUBCOMMAND_TO_DESCRIPTION_MAP, COMMANDS_TO_DESCRIPTION_MAP
 
 def version_callback(value: bool):
     if value:
         typer.echo("Knee 1.0.0")
         raise typer.Exit()
 
-def help_callback(value: bool):
+def command_callback(value: bool):
     if value:
-        for category, data in COMMAND_WITH_DESCRIPTION.items():
+        for category, data in SUBCOMMAND_TO_DESCRIPTION_MAP.items():
             typer.echo(f"\n{category} :{data['description']}\n")
             max_length = max(len(command) for command in data['commands'].keys())
             for command, description in data['commands'].items():
                 typer.echo(f"{command:<{max_length}} : {description}")
+        raise typer.Exit()
+
+def help_callback(value: bool):
+    if value:
+        typer.echo(typer.style("Options:", fg=typer.colors.BRIGHT_MAGENTA))
+        typer.echo("-v, --version : Show the version and exit.")
+        typer.echo("-c, --command : Show the commands and exit.")
+        typer.echo("-h, --help : Show this message and exit.")
+        typer.echo()
+        typer.echo(typer.style("Commands:", fg=typer.colors.BRIGHT_MAGENTA))
+        for command, description in COMMANDS_TO_DESCRIPTION_MAP.items():
+            typer.echo(f"{command} : {description}")
         raise typer.Exit()
