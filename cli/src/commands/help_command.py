61515bb16f91f107febaded0295589f4d8805ff2
RageshwariR <rageshwari@shurutech.com>
2024-04-09 17:23:43 +0530

    [Akhil|Rageshwari] Update constant names in help command file

--- cli/src/commands/help_command.py
+++ cli/src/commands/help_command.py
@@ -1,5 +1,5 @@
 import typer
-from constants import SUBCOMMAND_TO_DESCRIPTION_MAP, COMMANDS_TO_DESCRIPTION_MAP
+from constants import SUBCOMMAND_DESCRIPTION_MAP, COMMANDS_DESCRIPTION_MAP
 
 def version_callback(value: bool):
     if value:
@@ -8,7 +8,7 @@ def version_callback(value: bool):
 
 def command_callback(value: bool):
     if value:
-        for category, data in SUBCOMMAND_TO_DESCRIPTION_MAP.items():
+        for category, data in SUBCOMMAND_DESCRIPTION_MAP.items():
             typer.echo(f"\n{category} :{data['description']}\n")
             max_length = max(len(command) for command in data['commands'].keys())
             for command, description in data['commands'].items():
@@ -23,6 +23,6 @@ def help_callback(value: bool):
         typer.echo("-h, --help : Show this message and exit.")
         typer.echo()
         typer.echo(typer.style("Commands:", fg=typer.colors.BRIGHT_MAGENTA))
-        for command, description in COMMANDS_TO_DESCRIPTION_MAP.items():
+        for command, description in COMMANDS_DESCRIPTION_MAP.items():
             typer.echo(f"{command} : {description}")
         raise typer.Exit()
