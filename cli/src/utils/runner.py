1e500e8c813e54c4b6538ddf771bc5328453cd80
RageshwariR <rageshwari@shurutech.com>
2024-04-12 11:45:41 +0530

    [Rageshwari] refactor runner.py remove unused code

--- cli/src/utils/runner.py
+++ cli/src/utils/runner.py
@@ -4,5 +4,3 @@ def run_playbook(playbook, environment):
     response  = ansible_runner.run(private_data_dir="./", playbook=f"playbooks/{playbook}", inventory=f"inventories/{environment}")
     if response.stats == "failed":
         raise Exception("Playbook failed")
-    else:
-        print("Playbook success")
